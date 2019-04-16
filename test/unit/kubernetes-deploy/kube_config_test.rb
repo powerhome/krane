7fe393f79ccb8b08e470a3935442c0ddaeba8841
Ben Langfeld <blangfeld@powerhrg.com>
2019-04-16 13:27:37 -0300

    Removes special treatment of GCP authentication
    https://github.com/abonas/kubeclient/pull/394#issuecomment-469012306

--- test/unit/kubernetes-deploy/kube_config_test.rb
+++ test/unit/kubernetes-deploy/kube_config_test.rb
@@ -4,48 +4,13 @@ require 'test_helper'
 class KubeConfigTest < KubernetesDeploy::TestCase
   def setup
     WebMock.disable_net_connect!
-    set_google_env_vars
   end
 
   def teardown
     WebMock.allow_net_connect!
   end
 
-  def test_auth_use_default_gcp_success
-    config = KubernetesDeploy::KubeclientBuilder::KubeConfig.new(kubeconfig, "")
-
-    stub_request(:post, 'https://oauth2.googleapis.com/token')
-      .to_return(
-        headers: { 'Content-Type' => 'application/json' },
-        body: {
-          "access_token" => "bearer_token",
-          "token_type" => "Bearer",
-          "expires_in" => 3600,
-          "id_token" => "identity_token",
-        }.to_json,
-        status: 200
-      )
-
-    context = config.context("google")
-    assert_equal('bearer_token', context.auth_options[:bearer_token])
-  end
-
-  def test_auth_use_default_gcp_failure
-    config = KubernetesDeploy::KubeclientBuilder::KubeConfig.new(kubeconfig, "")
-
-    stub_request(:post, 'https://oauth2.googleapis.com/token')
-      .to_return(
-        headers: { 'Content-Type' => 'application/json' },
-        body: '',
-        status: 401
-      )
-
-    assert_raises(Signet::AuthorizationError) do
-      config.context("google")
-    end
-  end
-
-  def test_non_google_auth_works
+  def test_username_password_auth_works
     config = KubernetesDeploy::KubeclientBuilder::KubeConfig.new(kubeconfig, "")
 
     context = config.context("minikube")
@@ -63,28 +28,12 @@ class KubeConfigTest < KubernetesDeploy::TestCase
       'contexts' => [
         {
           'context' => {
-            'cluster' => 'test',
-            'user' => 'google',
-          },
-          'name' => 'google',
-        },
-        {
-          'context' => {
             'cluster' => 'test', 'user' => 'minikube'
           },
           'name' => 'minikube',
         },
       ],
       'users' => [
-        {
-          'name' => 'google',
-          'user' => {
-            'auth-provider' => {
-              'name' => 'gcp',
-              'config' => { 'access_token' => 'test' },
-            },
-          },
-        },
         {
           'name' => 'minikube',
           'user' => {
@@ -94,14 +43,5 @@ class KubeConfigTest < KubernetesDeploy::TestCase
         },
       ],
     }.stringify_keys
-  end
-
-  def set_google_env_vars
-    ENV["GOOGLE_PRIVATE_KEY"] ||= "FAKE"
-    ENV["GOOGLE_CLIENT_EMAIL"] ||= "fake@email.com"
-    ENV["GOOGLE_ACCOUNT_TYPE"] ||= 'authorized_user'
-    ENV["GOOGLE_CLIENT_ID"] ||= 'fake'
-    ENV["GOOGLE_CLIENT_SECRET"] ||= 'fake'
-    ENV["REFRESH_TOKEN_VAR"] ||= 'fake'
   end
 end
